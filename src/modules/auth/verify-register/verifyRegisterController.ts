428a30ee26746868da2885f8c854332535912403
Edwin Ortiz <edarcode@gmail.com>
2024-10-14 16:50:38 -0500

    refactor

--- src/modules/auth/verify-register/verifyRegisterController.ts
+++ src/modules/auth/verify-register/verifyRegisterController.ts
@@ -6,8 +6,44 @@ export const verifyRegisterController: Controller = async (_req, res, next) => {
   try {
     const register = res.locals?.tokenPayload as Register;
     await verifyRegisterService(register);
-    res.status(201).json({ msg: "Cuenta creada correctamente." });
+    res.status(201).send(page);
   } catch (error) {
     next(error);
   }
 };
+
+const page = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Cuenta Creada</title>
+          <style>
+            body {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              height: 100vh;
+              margin: 0;
+              background-color: #f4f4f4;
+            }
+            a {
+              display: inline-block;
+              padding: 10px 20px;
+              background-color: #007bff;
+              color: #fff;
+              text-decoration: none;
+              border-radius: 5px;
+              transition: background-color 0.3s;
+            }
+            a:hover {
+              background-color: #0056b3;
+            }
+          </style>
+        </head>
+        <body>
+          <a href=${process.env.CLIENT_BASE_URL}>Ir al FRONT</a>
+        </body>
+      </html>
+    `;
